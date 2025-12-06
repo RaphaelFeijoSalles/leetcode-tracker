0aca64e70907462130a760d6d4c96c20148100d6
Raphael Salles <feijoraphael1@gmail.com>
2025-12-06 17:17:23 -0300

    fixing deploy problems

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // Ajuste importante para GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: "/professional-checklist-leetcode/",
+  base: "/Professional-checklist-leetcode/",
 });
