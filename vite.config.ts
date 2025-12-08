9452e32c72f7092ba7e3cc1e435befe8e28d3704
Raphael Salles <feijoraphael1@gmail.com>
2025-12-08 12:11:59 -0300

    chore: update repo name and build config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // Ajuste importante para GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: "/Professional-checklist-leetcode/",
+  base: "/leetcode-tracker/",
 });
